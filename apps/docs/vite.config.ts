d31e823af1e249fe7026304139235e1cbd917cbc
路狐羽 <75880483+aixcyi@users.noreply.github.com>
2026-01-23 16:46:47 +0800

    refactor: 优化脚本，使用vite插件打包构建产物

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -2,12 +2,19 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'node:path'
 import { defineConfig } from 'vite'
+import { viteZip } from 'vite-plugin-zip-file'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         tailwindcss(),
         vue(),
+        viteZip({
+            folderPath: path.resolve(__dirname, 'dist'),
+            outPath: path.resolve(__dirname),
+            zipName: 'dist.zip',
+            withoutMainFolder: true,
+        }),
     ],
     resolve: {
         alias: {
--- apps/home/vite.config.ts
+++ apps/home/vite.config.ts
@@ -1,7 +1,9 @@
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'node:path'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { viteZip } from 'vite-plugin-zip-file'
 
 
 const timestamp = Math.trunc(new Date().getTime() / 1000)
@@ -11,6 +13,12 @@ export default defineConfig({
     plugins: [
         tailwindcss(),
         vue(),
+        viteZip({
+            folderPath: path.resolve(__dirname, 'dist'),
+            outPath: path.resolve(__dirname),
+            zipName: 'dist.zip',
+            withoutMainFolder: true,
+        }),
     ],
     define: {
         '__APP_VERSION__': `"v${process.env.npm_package_version}+${timestamp}"`,
