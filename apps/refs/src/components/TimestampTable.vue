13825f43db47a4f8ef703cd685771ff9f1e30127
路狐羽 <75880483+aixcyi@users.noreply.github.com>
2026-03-18 15:36:58 +0800

    feat(refs): “时间戳对照表”添加自选列高亮

--- apps/refs/src/components/TimestampTable.vue
+++ apps/refs/src/components/TimestampTable.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
+import { Icon } from '@iconify/vue';
 import { Duration } from '@navifox/utils';
+import { ref } from 'vue';
 
 const props = defineProps<{
     title: string,
@@ -12,6 +14,7 @@ const props = defineProps<{
 
 type Data = { stamp: bigint, powers: Map<bigint, bigint>, delta: string, datetime: string }
 
+const columnHighlighted = ref<bigint>(0n)
 const bases = [ 2n, 8n, 10n, 16n, 24n, 26n, 32n, 36n, 62n, 64n, 85n ]
 const table = new Map<bigint, Data>()
 let power = 0n
@@ -45,26 +48,41 @@ const data = [ ...table.values() ].sort(
 
 <template>
 <div class="relative bg-white dark:bg-slate-800 rounded-lg overflow-hidden">
-    <h3 class="absolute opacity-90 right-0 top-0 z-10 rounded-bl-lg px-3 py-1 text-white text-xs/4 tracking-wider bg-gradient-to-r from-indigo-500 to-purple-600">
+    <h3 class="absolute opacity-90 left-0 top-0 z-10 rounded-br-lg px-3 py-1 text-white text-xs/4 tracking-wider bg-gradient-to-r from-indigo-500 to-purple-600">
         {{ title }}</h3>
     <table class="block overflow-x-auto text-slate-900 dark:text-slate-200">
         <thead class="**:[th]:px-4 **:[th]:py-2">
         <tr>
             <th class="text-right">存储上限</th>
-            <th class="text-right bg-slate-200 dark:bg-slate-700">时间上限</th>
-            <th v-for="() in bases" class="text-center"></th>
+            <th :class="columnHighlighted === 0n ? 'bg-slate-200 dark:bg-slate-700' : ''"
+                class="text-right">时间上限
+            </th>
+            <th v-for="base in bases"
+                :class="columnHighlighted === base ? 'bg-slate-200 dark:bg-slate-700' : ''"
+                class="text-center">
+                <button class="cursor-pointer"
+                        type="button"
+                        @click="columnHighlighted = columnHighlighted === base ? 0n : base">
+                    <Icon class="text-slate-300 dark:text-slate-600 hover:text-purple-500"
+                          height="24"
+                          icon="bx:current-location" />
+                </button>
+            </th>
         </tr>
         </thead>
         <tbody class="**:[td]:px-4 **:[td]:py-2">
         <tr v-for="row in data"
             class="hover:bg-purple-200 dark:hover:bg-purple-900 transition-colors duration-200">
             <td class="text-right" nowrap="nowrap" v-html="row.delta" />
-            <td class="text-right bg-slate-200 dark:bg-slate-700"
+            <td :class="columnHighlighted === 0n ? 'bg-slate-200 dark:bg-slate-700' : ''"
+                class="text-right"
                 nowrap="nowrap"><code>{{ row.datetime }}</code></td>
-            <td v-for="base in bases" class="text-center">
-            <span v-if="row.powers.get(base) !== undefined">
-                {{ base }}<sup>{{ row.powers.get(base) }}</sup>
-            </span>
+            <td v-for="base in bases"
+                :class="columnHighlighted === base ? 'bg-slate-200 dark:bg-slate-700' : ''"
+                class="text-center">
+                <span v-if="row.powers.get(base) !== undefined">
+                    {{ base }}<sup>{{ row.powers.get(base) }}</sup>
+                </span>
             </td>
         </tr>
         </tbody>
