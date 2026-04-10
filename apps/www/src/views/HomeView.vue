4dd6f6359cf5464b2f0e80b0941a3eb4bfc65386
路狐羽 <75880483+aixcyi@users.noreply.github.com>
2026-04-10 16:31:48 +0800

    refactor(www): 微调首页浅色模式颜色

--- apps/www/src/views/HomeView.vue
+++ apps/www/src/views/HomeView.vue
@@ -19,13 +19,13 @@ onClickOutside(dropdown, () => isShowingDropdown.value = false)
 
 <template>
 <img :src="Background" alt="背景图片" class="absolute size-full select-none object-cover z-0" />
-<div class="absolute size-full bg-black/67 z-10" />
+<div class="absolute size-full bg-black/33 dark:bg-black/67 z-10" />
 
 <div class="absolute right-0 z-30 w-full">
-    <div class="MaxContainer">
+    <div class="MaxContainer selection:bg-[#B5A2FD60] **:transition-colors **:duration-200">
         <div class="md:hidden flex justify-end">
             <button
-                class="p-5 cursor-pointer hover:text-cyan-600 dark:hover:text-[#B5A2FD] transition-colors duration-200"
+                class="p-5 cursor-pointer hover:text-purple-400 dark:hover:text-[#B5A2FD]"
                 @click="isShowingDropdown = true"
             >
                 <Icon height="28" icon="lineicons:menu" />
@@ -35,7 +35,7 @@ onClickOutside(dropdown, () => isShowingDropdown.value = false)
             <a
                 v-for="{meta, path} in router.options.routes.filter(r => r.meta?.showOnNavbar)"
                 :href="path"
-                class="px-6 py-5 text-gray-300 hover:text-yellow-100 hover:bg-[#B5A2FD]/33 transition-colors duration-200"
+                class="px-6 py-5 text-white font-bold hover:text-orange-200 dark:hover:text-orange-300 hover:bg-[#B5A2FDA0] dark:hover:bg-[#B5A2FD50]"
             >
                 {{ meta?.title ?? '首页' }}
             </a>
@@ -49,9 +49,10 @@ onClickOutside(dropdown, () => isShowingDropdown.value = false)
             v-if="isShowingDropdown"
             class="text-slate-900 dark:text-slate-200 bg-slate-200 dark:bg-slate-800 rounded-t-lg"
         >
-            <div class="MaxContainer flex flex-col items-end">
+            <div
+                class="MaxContainer flex flex-col items-end selection:bg-[#B5A2FD60] **:transition-colors **:duration-200">
                 <button
-                    class="p-5 cursor-pointer hover:text-cyan-600 dark:hover:text-[#B5A2FD] transition-colors duration-200"
+                    class="p-5 cursor-pointer hover:text-purple-400 dark:hover:text-[#B5A2FD]"
                     @click="isShowingDropdown = false"
                 >
                     <Icon height="28" icon="lineicons:close" />
@@ -60,7 +61,7 @@ onClickOutside(dropdown, () => isShowingDropdown.value = false)
                     <template v-for="({meta, path}, index) in router.options.routes.filter(r => r.meta?.showOnNavbar)">
                         <div v-if="index > 0" class="border-t border-t-slate-300 dark:border-t-slate-700" />
                         <a :href="path"
-                           class="p-5 text-nowrap hover:text-cyan-600 dark:hover:text-[#B5A2FD] transition-colors duration-200"
+                           class="p-5 text-nowrap hover:text-purple-400 dark:hover:text-[#B5A2FD]"
                            target="_blank">{{ meta?.title ?? '首页' }}</a>
                     </template>
                 </div>
@@ -69,14 +70,17 @@ onClickOutside(dropdown, () => isShowingDropdown.value = false)
     </Transition>
 </div>
 
-<div class="MaxContainer h-screen flex flex-col selection:bg-[#B5A2FD]/33 z-20 **:z-20">
+<div class="MaxContainer h-screen flex flex-col selection:bg-[#B5A2FD60] z-20 **:z-20">
     <div class="h-full mb-8 md:mb-20 flex flex-col justify-end">
-        <div class="text-xl md:text-2xl mb-4 text-[#B5A2FD]"><code>@{{ tighnari.uid }}</code></div>
+        <div
+            class="w-fit text-xl md:text-2xl mb-4 not-dark:px-2 py-1 rounded-md not-dark:bg-[#B5A2FDA0] dark:text-[#B5A2FD]">
+            <code>@{{ tighnari.uid }}</code>
+        </div>
         <div class="font-medium text-4xl md:text-6xl md:max-w-[75%]">
             <span class="text-white">{{ tighnari.name }}</span>
-            <span v-for="tag in tighnari.tags" class="text-gray-400">／{{ tag }}</span>
+            <span v-for="tag in tighnari.tags" class="text-gray-300/75 dark:text-gray-400">／{{ tag }}</span>
         </div>
-        <div class="text-4xl md:text-5xl mt-4 text-gray-400 font-sign" v-html="tighnari.description" />
+        <div class="text-4xl md:text-5xl mt-4 text-white dark:text-gray-400 font-sign" v-html="tighnari.description" />
     </div>
 </div>
 </template>
